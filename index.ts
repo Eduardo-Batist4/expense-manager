2c5ffe04219cfedd5b130a5ece6d0e0289d9a11d
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-10-31 21:07:20 -0300

    importing and configuring index

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import connectDB from './db/conn';
+import userRoutes from './routes/UserRoutes';
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(userRoutes)
 
-app.listen(5000);
+app.listen(8000);
