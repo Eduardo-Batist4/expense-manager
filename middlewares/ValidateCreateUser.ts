b259650ca1abb4f6077708694a83ddc8e7d92e24
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-11-02 15:40:15 -0300

    fields validator for createUser created

---
+++ middlewares/ValidateCreateUser.ts
@@ -0,0 +1,29 @@
+import { body, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+export const validateUser = [
+    body('name').notEmpty().withMessage('The Name is required!'),
+    body('email')
+        .notEmpty().withMessage('The Email is required!')
+        .isEmail().withMessage('The Invalid Email!'),
+    body('password')
+        .notEmpty().withMessage('The Password is required!')
+        .isLength({ min: 6 }).withMessage('The Password must be at 6 characters!'),
+    body('confirmPassword')
+        .notEmpty().withMessage('The ConfirmPassword is required!')
+        .custom((value, { req }) => {
+            if(value !== req.body.password) {
+              throw new Error("The Passwords don't match!");  
+            };
+            return true;
+        }),
+];
+
+export const handleValidateErros = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(400).json({ error: errors.array() });
+        return; 
+    };
+    next();
+};
