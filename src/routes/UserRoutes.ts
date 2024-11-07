0ee5a3a8139e67e297318b1963f0375a4f78d238
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-11-07 11:24:20 -0300

    adding middleware verifyToken to delete route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -6,9 +6,10 @@ import Usercontroller from '../controllers/UserController';
 
 // Import Middlewares
 import { validateUser, handleValidateErros } from '../middlewares/ValidateCreateUser'
+import { verifyToken } from '../middlewares/authMiddleware';
 
 routes.post('/users/register', validateUser, handleValidateErros, Usercontroller.createUser);
 routes.get('/users/:id', Usercontroller.getUserById);
-routes.delete('/users/:id', Usercontroller.deleteUser);
+routes.delete('/users/:id', verifyToken, Usercontroller.deleteUser);
 
 export default routes;
